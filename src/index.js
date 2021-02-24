57fe81a2aa7c241b7b6a576217c5e37505c76fec
areicol <78863753+areicol@users.noreply.github.com>
2021-02-24 11:55:20 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@
 
 import "./css/main.css";
 
-<script src="//assets.adobedtm.com/645cdc4519e1/90b00018a3d3/launch-2638a91f1f4f.min.js" async></script>
-
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
